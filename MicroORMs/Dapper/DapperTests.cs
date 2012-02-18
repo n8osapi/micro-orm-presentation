e7921d01be413e3e514eabdd6db2320ae7493314
Nathan Kelley <nathan@devSolo.com>
2012-02-18 17:45:05 -0500

    Added Dapper Paging

--- MicroORMs/Dapper/DapperTests.cs
+++ MicroORMs/Dapper/DapperTests.cs
@@ -35,7 +35,17 @@ namespace Dapper
         [Test]
         public void BasicPaging()
         {
-            //Nope
+            using (IDbConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Chinook"].ConnectionString))
+            {
+                conn.Open();
+                var artistsPage1 = conn.Query<Artist>("SELECT * FROM (SELECT ROW_NUMBER() OVER (ORDER BY ArtistId) AS Row, * FROM Artist ) AS Paged  WHERE Row >= @Start AND Row <=@End", new { Start = 1, End = 5 });
+                var artistsPage2 = conn.Query<Artist>("SELECT * FROM (SELECT ROW_NUMBER() OVER (ORDER BY ArtistId) AS Row, * FROM Artist ) AS Paged  WHERE Row >= @Start AND Row <=@End", new { Start = 6, End = 10 });
+                Assert.That(artistsPage1.Count(), Is.EqualTo(5));
+                Assert.That(artistsPage1.First().ArtistId, Is.EqualTo(1));
+                Assert.That(artistsPage2.Count(), Is.EqualTo(5));
+                Assert.That(artistsPage2.First().ArtistId, Is.EqualTo(6));
+            }
+            
         }
     }
 }
