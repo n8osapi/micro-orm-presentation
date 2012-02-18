be4bba3c1abcd07155ce15cad694de8bc580d466
Nathan Kelley <nathan@devSolo.com>
2012-02-18 17:37:35 -0500

    Dapper

---
+++ MicroORMs/Dapper/DapperTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit;
+using NUnit.Framework;
+using Dapper;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Dapper
+{
+    [TestFixture]
+    public class DapperTests
+    {
+        public class Artist
+        {
+            public int ArtistId { get; set; }
+            public string Name { get; set; }
+        }
+
+        [Test]
+        public void BasicSelect()
+        {
+            using (IDbConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Chinook"].ConnectionString))
+            {
+                conn.Open();
+                var artists = conn.Query<Artist>("SELECT * FROM Artist");
+                Assert.That(artists.Count(), Is.EqualTo(275));
+            }
+
+        }
+
+        [Test]
+        public void BasicPaging()
+        {
+            //Nope
+        }
+    }
+}
--- MicroORMs/Dapper/Properties/AssemblyInfo.cs
+++ MicroORMs/Dapper/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Dapper")]
+[assembly: AssemblyTitle("DapperX")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Dapper")]
+[assembly: AssemblyProduct("DapperX")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
