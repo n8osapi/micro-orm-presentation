552120f4d48460bd254e18ccd3367ed5bd725dfe
Nathan Kelley <nathan@devSolo.com>
2012-02-18 16:07:55 -0500

    massive

---
+++ MicroORMs/Massive/MassiveTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit;
+using NUnit.Framework;
+
+namespace Massive
+{
+
+
+
+    [TestFixture]
+    public class MassiveTests
+    {
+        [Test]
+        public void BasicSelect()
+        {
+            dynamic db = Massive.DB.Current;
+            var artists = db.Query("SELECT * FROM Artist");
+        }
+
+        [Test]
+        public void BasicPaging()
+        {
+            dynamic artists = new DynamicModel("Chinook", "Artist", "ArtistId");
+            dynamic artistsPage2 = artists.Paged(currentPage: 2, pageSize: 5);
+            foreach( dynamic item in artistsPage2.Items) {};
+        }
+    }
+}
